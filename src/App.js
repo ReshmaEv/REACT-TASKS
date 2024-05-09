9ae5f88b45a7e3fc8ffefc4bb5c46d808e0ae213
ReshmaEv <146925219+ReshmaEv@users.noreply.github.com>
2024-05-09 13:06:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from 'react-bootstrap/Card';
+
+
 
 function App() {
+  const data = [
+    {
+      id: 1,
+      name: 'adam johnson',
+      job: 'Developer',
+      image: 'https://t4.ftcdn.net/jpg/05/58/89/69/240_F_558896972_un5SuLBtmocbehvKQVfrKt95CEZ08JV9.jpg'
+
+    }, {
+      id: 2,
+      name: "linda larson",
+      job: 'Manager',
+      image: 'https://t3.ftcdn.net/jpg/07/03/43/84/240_F_703438425_IRIFnsJ7txpG803yNhnnHlRHKTBqAiIs.jpg'
+    },
+    {
+      id: 3,
+      name: "marry hudson",
+      job: "designer",
+      image: 'https://t4.ftcdn.net/jpg/03/28/94/79/240_F_328947974_26fQsrAPA5cLoL9fSfWZhLM58AQO6rCz.jpg'
+    },
+    {
+      id: 4,
+      name: "nina hudson",
+      job: "designer",
+      image: 'https://t4.ftcdn.net/jpg/05/93/92/73/240_F_593927375_w8abSGDLkkXzr8AbYiWPTE01BRwfkxCh.jpg'
+    },
+    {
+      id: 5,
+      name: "margo larson",
+      job: 'designer',
+      image: 'https://t4.ftcdn.net/jpg/05/58/89/69/240_F_558896972_un5SuLBtmocbehvKQVfrKt95CEZ08JV9.jpg'
+    },
+    {
+      id: 6,
+      name: 'nocole scavo',
+      job: "designer",
+      image: 'https://t3.ftcdn.net/jpg/05/74/56/28/240_F_574562866_WCOjxYSt0vDlaBFpcjg1v17BjAxC64Ze.jpg'
+    }
+  ]
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        data.map((p) => (
+          <>
+            <Card  key={p.id} style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={p.image} />
+              <Card.Body>
+                <Card.Title>{p.name}</Card.Title>
+                <Card.Text>{p.job}</Card.Text>
+              </Card.Body>
+            </Card>
+
+
+          </>
+        ))
+      }
+
+
+
+
     </div>
+
+
+
+
+
+
   );
 }
 
